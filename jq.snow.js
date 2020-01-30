47816d0e47905efd0faadd1a32a0b24d741f94db
zhaoqing <zhaoqing@chances.com.cn>
2020-01-30 11:47:07 +0800

    'snow_update'

--- jq.snow.js
+++ jq.snow.js
@@ -8,10 +8,11 @@
       defaults = { minSize: 10, maxSize: 20, newOn: 1000, flakeColor: "#FFFFFF" },
       options = $.extend({}, defaults, options);
     var interval = setInterval(function () {
-      var startPositionLeft = Math.random() * documentWidth - 100, startOpacity = 0.5 + Math.random(),
+      var startPositionLeft = Math.random() * documentWidth - 100,
+        startOpacity = 0.5 + Math.random(),
         sizeFlake = options.minSize + Math.random() * options.maxSize,
         endPositionTop = documentHeight - 40,
-        endPositionLeft = startPositionLeft - 100 + Math.random() * 100,
+        endPositionLeft = startPositionLeft - 10 + Math.random() * 100,
         durationFall = documentHeight * 10 + Math.random() * 5000;
       $flake.clone().appendTo('body')
         .css({ left: startPositionLeft, opacity: startOpacity, 'font-size': sizeFlake, color: options.flakeColor })
